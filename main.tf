12f22e7425890bcc199cdf641bf4868c38dc3114
natalia.gassis <natalia.gassis@luizalabs.com>
2025-05-26 23:23:17 -0300

    Atualiza variáveis de ambiente e parâmetros do módulo MGC SSH Keys no Terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-module "magalu_kubernetes_cluster" {
-  source = "github.com/terraform-mgc-modules/mgc-ssh-keys?ref=v2.0.0"
-  name   = var.ssh_key_name
-  key    = var.ssh_key_value
+module "mgc_ssh_keys" {
+  source         = "github.com/terraform-mgc-modules/mgc-ssh-keys?ref=v2.0.0"
+  ssh_key_value  = var.ssh_key_value
+  mgc_api_key    = var.mgc_api_key
+  mgc_key_id     = var.mgc_key_id
+  mgc_key_secret = var.mgc_key_secret
 
 }
